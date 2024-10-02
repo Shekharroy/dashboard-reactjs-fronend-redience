5b6fe6ff2d1539c92e3c2752bf2f5a5bdc37c6ce
Chandrashekhar <chandrashekharroy4u@gmail.com>
2024-10-02 15:42:29 +0530

    sidebar commit

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -4,13 +4,16 @@ import { Button } from "@mui/material";
 import { RiDashboardHorizontalFill } from "react-icons/ri";
 import { FaAngleRight } from "react-icons/fa";
 import { FaUnlockAlt } from "react-icons/fa";
-import { MdOutlineMessage } from "react-icons/md";
+import { MdMessage } from "react-icons/md";
 import { IoNotifications } from "react-icons/io5";
-import { LiaFileInvoiceSolid } from "react-icons/lia";
+import { FaFileAlt } from "react-icons/fa";
 import { FaUser } from "react-icons/fa";
-import { IoLogInOutline } from "react-icons/io5";
+import { IoLogInSharp } from "react-icons/io5";
 import { SiGnuprivacyguard } from "react-icons/si";
 import { IoMdSettings } from "react-icons/io";
+
+import { Link } from "react-router-dom";
+
 const Sidebar = () => {
   return (
     <>
@@ -19,82 +22,100 @@ const Sidebar = () => {
             {/** */}
 
           <li>
-            <Button className="w-100">
-              <span className="icon"><RiDashboardHorizontalFill/></span>Dashboard
-              <span className="arrowIcon"><FaAngleRight/></span>
-            </Button>
+            <Link to="/">
+                <Button className="w-100">
+                <span className="icon"><RiDashboardHorizontalFill/></span>Dashboard
+                <span className="arrowIcon"><FaAngleRight/></span>
+                </Button>
+            </Link>
           </li>
 
             {/** */}
 
           <li>
-            <Button className="w-100">
-              <span className="icon"><FaUnlockAlt/></span>Authentications
-              <span className="arrowIcon"><FaAngleRight/></span>
-            </Button>
+            <Link to="/">
+                <Button className="w-100">
+                <span className="icon"><FaUnlockAlt/></span>Authentications
+                <span className="arrowIcon"><FaAngleRight/></span>
+                </Button>
+            </Link>
           </li>
             
             {/** */}
 
           <li>
-            <Button className="w-100">
-              <span className="icon"><MdOutlineMessage/></span>Messages
-              <span className="arrowIcon"><FaAngleRight/></span>
-            </Button>
+            <Link to="/">
+                <Button className="w-100">
+                <span className="icon"><MdMessage/></span>Messages
+                <span className="arrowIcon"><FaAngleRight/></span>
+                </Button>
+            </Link>
           </li>
 
           {/** */}
 
           <li>
-            <Button className="w-100">
-              <span className="icon"><IoNotifications/></span>Notifications
-              <span className="arrowIcon"><FaAngleRight/></span>
-            </Button>
+            <Link to="/">
+                <Button className="w-100">
+                <span className="icon"><IoNotifications/></span>Notifications
+                <span className="arrowIcon"><FaAngleRight/></span>
+                </Button>
+            </Link>
           </li>
 
           {/** */}
 
           <li>
-            <Button className="w-100">
-              <span className="icon"><LiaFileInvoiceSolid/></span>Invoice
-              <span className="arrowIcon"><FaAngleRight/></span>
-            </Button>
+            <Link to="/">
+                <Button className="w-100">
+                <span className="icon"><FaFileAlt/></span>Invoice
+                <span className="arrowIcon"><FaAngleRight/></span>
+                </Button>
+            </Link>
           </li>
 
           {/** */}
 
           <li>
-            <Button className="w-100">
-              <span className="icon"><FaUser/></span>Users
-              <span className="arrowIcon"><FaAngleRight/></span>
-            </Button>
+            <Link to="/">
+                <Button className="w-100">
+                <span className="icon"><FaUser/></span>Users
+                <span className="arrowIcon"><FaAngleRight/></span>
+                </Button>
+            </Link>
           </li>
 
           {/** */}
 
           <li>
-            <Button className="w-100">
-              <span className="icon"><IoLogInOutline/></span>Login
-              <span className="arrowIcon"><FaAngleRight/></span>
-            </Button>
+            <Link to="/">
+                <Button className="w-100">
+                <span className="icon"><IoLogInSharp/></span>Login
+                <span className="arrowIcon"><FaAngleRight/></span>
+                </Button>
+            </Link>
           </li>
 
           {/** */}
 
           <li>
-            <Button className="w-100">
-              <span className="icon"><SiGnuprivacyguard/></span>Sign Up
-              <span className="arrowIcon"><FaAngleRight/></span>
-            </Button>
+            <Link to="/">
+                <Button className="w-100">
+                <span className="icon"><SiGnuprivacyguard/></span>Sign Up
+                <span className="arrowIcon"><FaAngleRight/></span>
+                </Button>
+            </Link>
           </li>
 
           {/** */}
 
           <li>
-            <Button className="w-100">
-              <span className="icon"><IoMdSettings/></span>Settings
-              <span className="arrowIcon"><FaAngleRight/></span>
-            </Button>
+            <Link to="/">
+                <Button className="w-100">
+                <span className="icon"><IoMdSettings/></span>Settings
+                <span className="arrowIcon"><FaAngleRight/></span>
+                </Button>
+            </Link>
           </li>
         </ul>
       </div>

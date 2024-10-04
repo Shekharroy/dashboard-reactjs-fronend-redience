fe618697f71f5a54865f71bd90e0225e0f6022a9
Chandrashekhar <chandrashekharroy4u@gmail.com>
2024-10-04 22:42:47 +0530

    Side Bar Menu with Toggle effect on dashboard page

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Sidebar.css";
 import { Button } from "@mui/material";
 import { RiDashboardHorizontalFill } from "react-icons/ri";
@@ -8,13 +8,25 @@ import { MdMessage } from "react-icons/md";
 import { IoNotifications } from "react-icons/io5";
 import { FaFileAlt } from "react-icons/fa";
 import { FaUser } from "react-icons/fa";
-import { IoLogInSharp } from "react-icons/io5";
+import { LuLogIn } from "react-icons/lu";
 import { SiGnuprivacyguard } from "react-icons/si";
 import { IoMdSettings } from "react-icons/io";
+import { LuLogOut } from "react-icons/lu";
 
 import { Link } from "react-router-dom";
 
 const Sidebar = () => {
+
+    const [activeTab, setActiveTab] = useState(0);
+
+     const [isToggle, setIsToggle] = useState(false);
+
+    const isOpenSubmenu = (index) => {
+        console.log("Index === ",index);
+        setActiveTab(index);
+        setIsToggle(!isToggle);
+    };
+
   return (
     <>
       <div className="sidebar">
@@ -23,7 +35,7 @@ const Sidebar = () => {
 
           <li>
             <Link to="/">
-                <Button className="w-100">
+                <Button className={`w-100 ${activeTab===0 && isToggle===true ? 'active': ''}`} onClick={()=>isOpenSubmenu(0)}>
                 <span className="icon"><RiDashboardHorizontalFill/></span>Dashboard
                 <span className="arrowIcon"><FaAngleRight/></span>
                 </Button>
@@ -34,7 +46,7 @@ const Sidebar = () => {
 
           <li>
             <Link to="/">
-                <Button className="w-100">
+                <Button className={`w-100 ${activeTab===1 && isToggle===true ? 'active': ''}`} onClick={()=>isOpenSubmenu(1)}>
                 <span className="icon"><FaUnlockAlt/></span>Authentications
                 <span className="arrowIcon"><FaAngleRight/></span>
                 </Button>
@@ -45,7 +57,7 @@ const Sidebar = () => {
 
           <li>
             <Link to="/">
-                <Button className="w-100">
+                <Button className={`w-100 ${activeTab===2 && isToggle===true ? 'active': ''}`} onClick={()=>isOpenSubmenu(2)}>
                 <span className="icon"><MdMessage/></span>Messages
                 <span className="arrowIcon"><FaAngleRight/></span>
                 </Button>
@@ -56,7 +68,7 @@ const Sidebar = () => {
 
           <li>
             <Link to="/">
-                <Button className="w-100">
+                <Button className={`w-100 ${activeTab===3 && isToggle===true ? 'active': ''}`} onClick={()=>isOpenSubmenu(3)}>
                 <span className="icon"><IoNotifications/></span>Notifications
                 <span className="arrowIcon"><FaAngleRight/></span>
                 </Button>
@@ -67,7 +79,7 @@ const Sidebar = () => {
 
           <li>
             <Link to="/">
-                <Button className="w-100">
+                <Button className={`w-100 ${activeTab===4 && isToggle===true ? 'active': ''}`} onClick={()=>isOpenSubmenu(4)}>
                 <span className="icon"><FaFileAlt/></span>Invoice
                 <span className="arrowIcon"><FaAngleRight/></span>
                 </Button>
@@ -77,20 +89,25 @@ const Sidebar = () => {
           {/** */}
 
           <li>
-            <Link to="/">
-                <Button className="w-100">
+            <Button className={`w-100 ${activeTab===5  && isToggle===true ? 'active': ''}`} onClick={()=>isOpenSubmenu(5)}>
                 <span className="icon"><FaUser/></span>Users
                 <span className="arrowIcon"><FaAngleRight/></span>
-                </Button>
-            </Link>
+            </Button>
+            <div className={`submenuWrapper ${activeTab===5 && isToggle===true ? 'colapse':'colapsed'}`}>
+                <ul className="subMenu">
+                    <li><Link to="#">Administrators </Link></li>
+                    <li><Link to="#">Operators </Link></li>
+                    <li><Link to="#">Ordinary Users </Link></li>
+                </ul> 
+            </div>   
           </li>
 
           {/** */}
 
           <li>
             <Link to="/">
-                <Button className="w-100">
-                <span className="icon"><IoLogInSharp/></span>Login
+                <Button className={`w-100 ${activeTab===6  && isToggle===true ? 'active': ''}`} onClick={()=>isOpenSubmenu(6)}>
+                <span className="icon"><LuLogIn /></span>Login
                 <span className="arrowIcon"><FaAngleRight/></span>
                 </Button>
             </Link>
@@ -100,7 +117,7 @@ const Sidebar = () => {
 
           <li>
             <Link to="/">
-                <Button className="w-100">
+                <Button className={`w-100 ${activeTab===7  && isToggle===true ? 'active': ''}`} onClick={()=>isOpenSubmenu(7)}>
                 <span className="icon"><SiGnuprivacyguard/></span>Sign Up
                 <span className="arrowIcon"><FaAngleRight/></span>
                 </Button>
@@ -111,13 +128,21 @@ const Sidebar = () => {
 
           <li>
             <Link to="/">
-                <Button className="w-100">
+                <Button className={`w-100 ${activeTab===8  && isToggle===true ? 'active': ''}`} onClick={()=>isOpenSubmenu(8)}>
                 <span className="icon"><IoMdSettings/></span>Settings
                 <span className="arrowIcon"><FaAngleRight/></span>
                 </Button>
             </Link>
           </li>
         </ul>
+
+        {/** Logout button */}
+
+        <div className="logoutWrapper mt-5 mb-1">
+            <div className="logoutBox">
+                <Button variant="contained"><LuLogOut/> Logout</Button>
+            </div>
+        </div>
       </div>
     </>
   );
